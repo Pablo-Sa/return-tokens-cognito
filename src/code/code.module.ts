66b5dcdc753e06f72dadcd036daeb3cdf87e57b8
Pablo Vinicius Pereira de Sa <pablovp@ciandt.com>
2022-05-05 00:32:50 -0300

    Configurado HttpModule e DotEnv

--- src/code/code.module.ts
+++ src/code/code.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CodeService } from './code.service';
 import { CodeController } from './code.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [CodeController],
   providers: [CodeService]
 })
--- src/code/code.service.ts
+++ src/code/code.service.ts
@@ -1,9 +1,16 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { CodeDto } from './dto/create-code.dto';
 @Injectable()
 export class CodeService {
-  returnTokens(code: CodeDto) {
+
+  constructor(private httpService: HttpService) {}
+
+  async returnTokens(code: CodeDto) {
     console.log(code)
+    const teste = await firstValueFrom(this.httpService.get(process.env.EXAMPLE));
+    console.log(teste)
     return 'This action adds a new code';
   }
 }
